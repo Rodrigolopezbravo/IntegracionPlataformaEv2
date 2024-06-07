c157e60368eb625a5694f1450f89bb5359449916
rodrigolopezbravo6 <rodrigolopezbravo6@gmail.com>
2024-06-07 19:38:56 -0400

    Se genera api clientes y api productos

---
+++ ClienteApi/ClienteApi/Controllers/ClientesController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CustomerApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ClientesController : ControllerBase
+    {
+        private static IList<Cliente> clientes = new List<Cliente>
+        {
+            new Cliente { Id = 1, Name = "Rodrigo Lopez", Email = "rodrigolopez@duocuc.cl" },
+            new Cliente { Id = 2, Name = "Nicolas Chavez", Email = "nicolaschavez@duocuc.cl" },
+            new Cliente { Id = 3, Name = "Felipe Millan", Email = "felipemillan@duocuc.cl" }
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Cliente>> GetClientes()
+        {
+            return Ok(clientes);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Cliente> GetCliente(int id)
+        {
+            var cliente = clientes.FirstOrDefault(c => c.Id == id);
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+            return Ok(cliente);
+        }
+
+        [HttpPost]
+        public ActionResult<Cliente> CrearCliente([FromBody] Cliente cliente)
+        {
+            if (cliente == null)
+            {
+                return BadRequest("Cliente nulo.");
+            }
+            cliente.Id = clientes.Max(c => c.Id) + 1;
+            clientes.Add(cliente);
+            return CreatedAtAction(nameof(GetCliente), new { id = cliente.Id }, cliente);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult UpdateCliente(int id, [FromBody] Cliente cliente)
+        {
+            if (cliente == null)
+            {
+                return BadRequest("Cliente nulo.");
+            }
+
+            var existeCliente = clientes.FirstOrDefault(c => c.Id == id);
+            if (existeCliente == null)
+            {
+                return NotFound();
+            }
+            existeCliente.Name = cliente.Name;
+            existeCliente.Email = cliente.Email;
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteCliente(int id)
+        {
+            var cliente = clientes.FirstOrDefault(c => c.Id == id);
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+            clientes.Remove(cliente);
+            return NoContent();
+        }
+    }
+
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ ClienteApi/ClienteApi/Controllers/ProductosController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProductoApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductosController : ControllerBase
+    {
+        private static IList<Producto> productos = new List<Producto>
+        {
+            new Producto { Id = 1, Name = "PC", Price = 1200000 },
+            new Producto { Id = 2, Name = "Celular", Price = 500000 },
+            new Producto { Id = 3, Name = "Notebook", Price = 800000 }
+
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Producto>> GetProductos()
+        {
+            return Ok(productos);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Producto> GetProducto(int id)
+        {
+            var product = productos.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public ActionResult<Producto> CreateProducto([FromBody] Producto producto)
+        {
+            if (producto == null)
+            {
+                return BadRequest("Producto nulo.");
+            }
+            producto.Id = productos.Max(p => p.Id) + 1;
+            productos.Add(producto);
+            return CreatedAtAction(nameof(GetProducto), new { id = producto.Id }, producto);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult UpdateProducto(int id, [FromBody] Producto producto)
+        {
+            if (producto == null)
+            {
+                return BadRequest("Producto nulo.");
+            }
+
+            var existeProducto = productos.FirstOrDefault(p => p.Id == id);
+            if (existeProducto == null)
+            {
+                return NotFound();
+            }
+            existeProducto.Name = producto.Name;
+            existeProducto.Price = producto.Price;
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteProducto(int id)
+        {
+            var producto = productos.FirstOrDefault(p => p.Id == id);
+            if (producto == null)
+            {
+                return NotFound();
+            }
+            productos.Remove(producto);
+            return NoContent();
+        }
+    }
+
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
